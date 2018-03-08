37e64118b3804b52cfebf7cf4723089e8892b7d3
haley <zhuhe212@163.com>
2018-03-08 23:17:37 +0800

    support setting log level when starting

--- start.py
+++ start.py
@@ -1,7 +1,15 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
+import sys
+
 from editor import VmodelEditor
 
+log_level = logging.WARNING
+if len(sys.argv) > 1 and sys.argv[1].lower() == '--debug':
+	log_level = logging.DEBUG
+if len(sys.argv) > 1 and sys.argv[1].lower() == '--info':
+	log_level = logging.INFO
+
+logging.basicConfig(level=log_level)
 
 editor = VmodelEditor()
 editor.mainloop()
